616734705eb12b92332e42a3182ead9a5f48d8c1
Jahid Hasan <jahidhasanimonn@gmail.com>
2025-08-11 20:37:45 +0600

    fix: Add better responsibility delegation in service-repo layers
    - `healthCheck.service.ts` should handle all the business logic and
      error catching
    - `healthCheck.repo.ts` should only communicate with DB, should not know
      about errors

--- apps/api/src/services/healthCheck.service.ts
+++ apps/api/src/services/healthCheck.service.ts
@@ -1,13 +1,25 @@
 import logger from "@/config/logger";
+import { DATABASE_URL } from "@/config/secrets";
 import { healthCheckRepo } from "@/repositories";
+import { DrizzleError, DrizzleQueryError } from "drizzle-orm";
 
 export default class HealthCheckService {
   async dbConnectionCheck(): Promise<void> {
+    logger.info(DATABASE_URL);
     try {
       await healthCheckRepo.dbSelectOne();
-      logger.logger.info("Database connected successfully");
+      logger.info("Database connected successfully");
     } catch (error) {
-      logger.logger.error("Database connection failed");
+      if (error instanceof DrizzleError) {
+        logger.error({ error: error }, "Database connection error: ");
+      } else if (error instanceof DrizzleQueryError) {
+        logger.error({ error: error }, "Database query error: ");
+      } else if (error instanceof Error) {
+        logger.error(
+          { error: error },
+          "Unexpected error during database check: ",
+        );
+      }
     }
   }
 }
